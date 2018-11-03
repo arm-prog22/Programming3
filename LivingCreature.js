54493fcaa577efe096b434a993c0609a29a24fa7
arm-prog22 <karopoghosyan22@gmail.com>
2018-11-03 12:49:51 +0400

    added a new class : LivingCreature

---
+++ LivingCreature.js
@@ -0,0 +1,36 @@
+class LivingCreature{
+    constructor(x,y,index,directions){
+        this.x=x;
+        this.y=y;
+        this.index=index;
+        this.getNewCoords();
+    }
+
+    getNewCoords() {
+        this.directions = [
+          [this.x - 1, this.y - 1],
+          [this.x, this.y - 1],
+          [this.x + 1, this.y - 1],
+          [this.x - 1, this.y],
+          [this.x + 1, this.y],
+          [this.x - 1, this.y + 1],
+          [this.x, this.y + 1],
+          [this.x + 1, this.y + 1]
+        ];
+      }
+
+      chooseCell(character) {
+        this.getNewCoords();
+        var found = [];
+        for (var i in this.directions) {
+          var x = this.directions[i][0];
+          var y = this.directions[i][1];
+          if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
+            if (matrix[y][x] == character) {
+              found.push(this.directions[i]);
+            }
+          }
+        }
+        return found;
+      }
+}
