2c89ce6526f3ded765d0baa04d4cc955c61e3fad
arm-prog22 <karopoghosyan22@gmail.com>
2018-11-03 13:16:02 +0400

    integrating inheritance to Alien.js

--- Alien.js
+++ Alien.js
@@ -1,8 +1,6 @@
-class Alien {
+class Alien extends LivingCreature {
     constructor(x, y, index) {
-      this.x = x;
-      this.y = y
-      this.index = index;
+      super(x,y,index);
       this.counter = 0;
     }
   
@@ -19,22 +17,7 @@ class Alien {
         }
       }
   
-       chooseCell(character) {
-        this.getNewCoords();
-        var found = [];
-        for (var i in this.directions) {
-          var x = this.directions[i][0];
-          var y = this.directions[i][1];
-          if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
-            //console.log(y, x);
-            if (matrix[y][x] == character) {
-              found.push(this.directions[i]);
-            }
-          }
-        }
-        return found;
-      }
-    
+  
       followBlackHole(){
         var blackHole=random(this.chooseCell(5));
         matrix[this.y][this.x]=0;
